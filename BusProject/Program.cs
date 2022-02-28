8ae9bd18fc8ad9447ce5ed82ab635be6a0795817
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-02-28 10:36:54 +0300

    Add methods addBus and andStops

--- BusProject/Program.cs
+++ BusProject/Program.cs
@@ -12,88 +12,129 @@ namespace BusProject
     class Program
     {
         private static string _fileName = string.Empty;
-        private static DataTableCollection tableCollection = null;
         
         static void Main(string[] args)
         {
             Dictionary<string, HashSet<string>> Bus_Stop = new Dictionary<string, HashSet<string>>();
             Dictionary<string, HashSet<string>> Stop_Bus = new Dictionary<string, HashSet<string>>();
-            List <Bus> bus = addBus();
-            
-            _fileName = @"C:\Users\nasur\Desktop\рпм.xlsx";
+
+            //_fileName = @"C:\Users\nasur\Desktop\рпм.xlsx";
+
+            //Excel.Application ObjWorkExcel = new Microsoft.Office.Interop.Excel.Application();
+            //Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(_fileName);
+            //Excel.Worksheet ObjWorkSheet = ObjWorkBook.Sheets[1]; //получить 1-й лист
+            //var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
+
+            //Excel.Range forYach;
+
+            //int lastColumn = (int)lastCell.Column;
+            //int lastRow = (int)lastCell.Row;
+            //string[,] bus_stop = new string[lastRow, lastColumn];
+            //for (int j = 0; j < 2; j++)
+            //{
+            //    //по всем колонкам
+            //    for (int i = 0; i < 161; i++)
+            //    {
+            //        // по всем строкам
+            //        forYach = ObjWorkSheet.Cells[i + 1, j + 1] as Excel.Range;
+            //        bus_stop[i, j] = forYach.Value2.ToString();
+            //    }
+            //}
+            //foreach (var i in bus_stop)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Excel.Worksheet ObjWorkSheet2 = ObjWorkBook.Sheets[2]; //получить 1-й лист
+            //var lastCell2 = ObjWorkSheet2.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
+
+            //Excel.Range forYach2;
+
+            //int lastColumn2 = (int)lastCell2.Column;
+            //int lastRow2 = (int)lastCell2.Row;
+            //string[,] stop_bus = new string[lastRow2, lastColumn2];
+            //for (int j = 0; j < 2; j++)
+            //{
+            //    //по всем колонкам
+            //    for (int i = 0; i < lastRow2; i++)
+            //    {
+            //        // по всем строкам
+            //        forYach2 = ObjWorkSheet2.Cells[i + 1, j + 1] as Excel.Range;
+            //        stop_bus[i, j] = forYach2.Value2.ToString();
+            //    }
+            //}
+
+            //foreach (var i in stop_bus)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //List<Bus> buses = new List<Bus>();
+            //buses = AddBus();
+            //foreach(var i in buses)
+            //{
+            //    Console.WriteLine(i.id_bus);
+            //}
+
+            List<Stop> stops = new List<Stop>();
+            stops = AddStop();
+            foreach (var i in stops)
+            {
+                Console.Write($"{i.id_stop} {i.stopName}");
+            }
+        }
+
+        static List<Stop> AddStop()
+        {
+            List<Stop> buses = new List<Stop>();
+            _fileName = @"C:\Users\nasur\Desktop\КопияРпм.xlsx";
 
             Excel.Application ObjWorkExcel = new Microsoft.Office.Interop.Excel.Application();
             Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(_fileName);
-            Excel.Worksheet ObjWorkSheet = ObjWorkBook.Sheets[1]; //получить 1-й лист
+            Excel.Worksheet ObjWorkSheet = ObjWorkBook.Sheets[4]; //получить 3-й лист
             var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
 
             Excel.Range forYach;
-
-            int lastColumn = (int)lastCell.Column;
-            int lastRow = (int)lastCell.Row;
-            string[,] bus_stop = new string[lastRow, lastColumn];
-            for (int j = 0; j < 2; j++)
-            {
-                //по всем колонкам
-                for (int i = 0; i < lastRow; i++)
-                {
-                    // по всем строкам
-                    forYach = ObjWorkSheet.Cells[i + 1, j + 1] as Excel.Range;
-                    bus_stop[i, j] = forYach.Value2.ToString();
-                }
-            }
-            foreach (var i in bus_stop)
-            {
-                Console.WriteLine(i);
-            }
-
-
-            Excel.Worksheet ObjWorkSheet2 = ObjWorkBook.Sheets[2]; //получить 1-й лист
-            var lastCell2 = ObjWorkSheet2.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
-
             Excel.Range forYach2;
-
-            int lastColumn2 = (int)lastCell2.Column;
-            int lastRow2 = (int)lastCell2.Row;
-            string[,] stop_bus = new string[lastRow2, lastColumn2];
-            for (int j = 0; j < 2; j++)
+            int lastRow = (int)lastCell.Row;
+            for (int i = 0; i < 621; i++)
             {
-                //по всем колонкам
-                for (int i = 0; i < lastRow2; i++)
+                forYach = ObjWorkSheet.Cells[i + 1, 1] as Excel.Range;
+                forYach2 = ObjWorkSheet.Cells[i + 1, 2] as Excel.Range;
+                Stop bus = new Stop()
                 {
-                    // по всем строкам
-                    forYach2 = ObjWorkSheet2.Cells[i + 1, j + 1] as Excel.Range;
-                    stop_bus[i, j] = forYach2.Value2.ToString();
-                }
+                    id_stop = Convert.ToInt32(forYach.Value2),
+                    stopName = forYach2.Value2.ToString()
+                };
+                buses.Add(bus);
             }
-
-            foreach (var i in stop_bus)
-            {
-                Console.WriteLine(i);
-            }
-
+            return buses;
         }
 
-        private static void OpenExcelFile(string FilePath)
-        {
-            FileStream stream = File.Open(FilePath, FileMode.Open, FileAccess.Read);
-
-            IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
-
-            DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration()
-            {
-                ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
-                {
-                    UseHeaderRow = false
-                }
-            }) ;
-
-            tableCollection = db.Tables;
-        }
-
-        static List<Bus> addBus()
+        static List<Bus> AddBus()
         {
             List<Bus> buses = new List<Bus>();
+            _fileName = @"C:\Users\nasur\Desktop\КопияРпм.xlsx";
+
+            Excel.Application ObjWorkExcel = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(_fileName);
+            Excel.Worksheet ObjWorkSheet = ObjWorkBook.Sheets[3]; //получить 3-й лист
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
+
+            Excel.Range forYach;
+            int lastRow = (int)lastCell.Row;
+
+            for (int i = 0; i < lastRow; i++)
+            {
+                forYach = ObjWorkSheet.Cells[i + 1, 1] as Excel.Range;
+                Bus bus = new Bus()
+                {
+                    id_bus = forYach.Value2.ToString()
+                };
+                buses.Add(bus);
+            }
+            
             return buses;
         }
     }
