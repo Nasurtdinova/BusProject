2ea1dff9e60844f523f590552f7e5b41d8b82d07
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-03-23 10:12:06 +0300

    Finish project

--- BusUnitTest/BusTest.cs
+++ BusUnitTest/BusTest.cs
@@ -66,12 +66,13 @@ namespace BusUnitTest
         public void TestAllBusses()
         {
             Bus bus = new Bus();
+            AllBuses allBuses = new AllBuses(bus);
+            Assert.AreEqual(allBuses.ToString(), "No buses");
 
             string line = "Tolstopaltsevo Marushkino Vnukovo";
             string line2 = "Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo";
             bus.AddNewBus("32", 3, line.Split());
             bus.AddNewBus("32K", 6, line2.Split());
-            AllBuses allBuses = new AllBuses(bus);
             
             List<string> allBussesCheck = new List<string>();
             allBussesCheck.Add("Bus 32: Tolstopaltsevo Marushkino Vnukovo");
@@ -95,25 +96,13 @@ namespace BusUnitTest
             bus.AddNewBus("32", 3, line.Split());
             bus.AddNewBus("32K", 6, line2.Split());
 
-            // because HashSet objects will be compared with object.Equals and they are not same
-            // https://stackoverflow.com/questions/5194966/mstest-collectionassert-areequivalent-failed-the-expected-collection-contains
             CollectionAssert.AreEqual(testDict, bus.bus_stop, new DictComparer());
-
-            // compare each HashSet manually
-            //foreach (var word in synonyms.Dict)
-            //{
-            //    CollectionAssert.AreEqual(testDict[word.Key], word.Value);
-            //}
         }
 
-        //Why CollectionAssert.AreEqual fails even when both lists contain the same items
-        //https://softwareonastring.com/357/why-collectionassert-areequal-fails-even-when-both-lists-contain-the-same-items
         private class DictComparer : Comparer<KeyValuePair<string, HashSet<string>>>
         {
             public override int Compare(KeyValuePair<string, HashSet<string>> lhs, KeyValuePair<string, HashSet<string>> rhs)
             {
-                // compare the two pais
-                // for the purpose of this tests they are considered equal when their identifiers (names) match
                 return lhs.Key.CompareTo(lhs.Key);
             }
         }
